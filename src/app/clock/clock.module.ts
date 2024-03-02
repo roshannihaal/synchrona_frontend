dd9d1f64267f00383f8546e35e94c547d493906b
roshannihaal <roshan.nihaal18@gmail.com>
2024-03-02 17:28:16 +0530

    Added Progress Bar

--- src/app/clock/clock.module.ts
+++ src/app/clock/clock.module.ts
@@ -5,9 +5,10 @@ import { ClockRoutingModule } from './clock-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { DisplayComponent } from './display/display.component';
 import { CelestialLoaderComponent } from './celestial-loader/celestial-loader.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [HeaderComponent, DisplayComponent, CelestialLoaderComponent],
-    imports: [CommonModule, ClockRoutingModule],
+    imports: [CommonModule, ClockRoutingModule, SharedModule],
 })
 export class ClockModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProgressBarModule } from 'primeng/progressbar';
+
+@NgModule({
+    declarations: [],
+    imports: [CommonModule, ProgressBarModule],
+    exports: [ProgressBarModule],
+})
+export class SharedModule {}
